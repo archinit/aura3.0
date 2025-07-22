73e189443ca6c5b3ac928a8f02fa391b8fecba59
ArchitGitHubPro <archit.7537@gmail.com>
2025-07-22 16:30:25 +0530

    contexts

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,16 +1,22 @@
 'use client'
 
 import { ThemeProvider } from "next-themes";
+import { SessionProvider as NextAuthSessionProivder } from "next-auth/react";
+import { SessionProvider as CustomSessionProivder } from "@/lib/contexts/session-context";
 
 export const Providers = ({children}: {children: React.ReactNode}) => {
     return (
-        <ThemeProvider 
-        attribute="class" 
-        defaultTheme="system" 
-        enableSystem
-        disableTransitionOnChange
-        >
-            {children}
-        </ThemeProvider>
+        <NextAuthSessionProivder>
+            <CustomSessionProivder>
+                <ThemeProvider 
+                attribute="class" 
+                defaultTheme="system" 
+                enableSystem
+                disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </CustomSessionProivder>
+        </NextAuthSessionProivder>
     )
 }
