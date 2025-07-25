8d62efa8ad952dc3ba313a75319af1c1b216ec89
ArchitGitHubPro <archit.7537@gmail.com>
2025-07-25 22:28:17 +0530

    minor fixes

---
+++ app/api/auth/session/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export async function GET(req: NextRequest) {
+    try {
+        const sessionToken = req.headers.get("Authorization")?.split(" ")[1];
+
+        if (!sessionToken) {
+            return NextResponse.json(
+                { isAuthenticated: false, user: null },
+                { status: 401 }
+            );
+        }
+
+        const decoded = jwt.verify
+    } catch (error) {
+        
+    }
+}
